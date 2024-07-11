b98f9a562ce6ce0e33099c7c992346b21839610d
charmingruby <mrrubycanal3105@gmail.com>
2024-07-11 10:52:41 -0300

    feat: rest server

--- api/cmd/api/main.go
+++ api/cmd/api/main.go
@@ -1,16 +1,25 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"log/slog"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/charmingruby/brandwl/config"
 	"github.com/charmingruby/brandwl/internal/domain/search/search_usecase"
-	"github.com/charmingruby/brandwl/internal/infra/database/mongo"
+	mongoRepo "github.com/charmingruby/brandwl/internal/infra/database/mongo"
+	"github.com/charmingruby/brandwl/internal/infra/transport/rest"
 	mongoConn "github.com/charmingruby/brandwl/pkg/mongo"
 	"github.com/charmingruby/brandwl/tests/fake"
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func main() {
@@ -33,6 +42,37 @@ func main() {
 		os.Exit(1)
 	}
 
-	searchRepo := mongo.NewMongoSearchRepository(db)
+	initDependencies(db)
+
+	router := gin.Default()
+	server := rest.NewServer(router, cfg.ServerConfig.Port)
+
+	go func() {
+		if err := server.Start(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			slog.Error(fmt.Sprintf("[REST SERVER] %s", err.Error()))
+			os.Exit(1)
+		}
+	}()
+
+	// Graceful shutdown
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM)
+	<-sc
+
+	slog.Info("HTTP Server interruption received!")
+
+	ctx, shutdownRelease := context.WithTimeout(context.Background(), 10*time.Second)
+	defer shutdownRelease()
+
+	if err := server.Server.Shutdown(ctx); err != nil {
+		slog.Error(fmt.Sprintf("[GRACEFUL SHUTDOWN REST SERVER] %s", err.Error()))
+		os.Exit(1)
+	}
+
+	slog.Info("Gracefully shutdown!")
+}
+
+func initDependencies(db *mongo.Database) {
+	searchRepo := mongoRepo.NewMongoSearchRepository(db)
 	search_usecase.NewSearchUseCase(searchRepo, fake.NewFakeGoogleAPI())
 }
